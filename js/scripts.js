21ff4e2bf1f649ba0b05c115d2b71e287c072d17
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-06-09 15:00:10 -0700

    Show or hide checkbox form, show or hide good or bad fortune to user depending on user input.
    Co-authored-by: Micheal Hansen <micheal.hansen@gmail.com>
    co-authored-by: Sean Downs <spdowns@protonmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,13 +2,26 @@ $(document).ready(function() {
   $("#fortune-form").submit(function(event) {
     event.preventDefault();
 
-    let superstition = [];
+    let superstitions = [];
     $("input:checkbox[name=superstition]:checked").each(function() {
       const userSuperstitions = $(this).val();
       superstitions.push(userSuperstitions);
     });
 
-    if (userSuperstitions.length > 1)
+    if (superstitions.length > 1) {
+      $(".bad-luck").show();
+    } else {
+      $(".good-luck").show();
+    }
+
+    $("#reset").show();
     $("#fortune-form").hide();
+  });
+
+  $("button#reset").click(function() {
+    $(".bad-luck").hide();
+    $(".good-luck").hide();
+    $("#fortune-form").show();
+    $("#reset").hide();
   });
 });
